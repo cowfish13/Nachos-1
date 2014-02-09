9e659cd3ed5c60bf371e91161cacca6c0ae715e4
nbjsw <nbxsjsw@yahoo.com>
2014-02-08 16:01:57 -0800

    Proj_1 Problem 3
    Implement Alarm

--- threads/Alarm.java
+++ threads/Alarm.java
@@ -2,11 +2,14 @@ package nachos.threads;
 
 import nachos.machine.*;
 
+import java.util.PriorityQueue;
+
 /**
  * Uses the hardware timer to provide preemption, and to allow threads to sleep
  * until a certain time.
  */
 public class Alarm {
+    private PriorityQueue<ThreadTimeTuple> waitQueue;
     /**
      * Allocate a new Alarm. Set the machine's timer interrupt handler to this
      * alarm's callback.
@@ -15,6 +18,7 @@ public class Alarm {
      * alarm.
      */
     public Alarm() {
+	this.waitQueue = new PriorityQueue<ThreadTimeTuple>();
 	Machine.timer().setInterruptHandler(new Runnable() {
 		public void run() { timerInterrupt(); }
 	    });
@@ -27,7 +31,16 @@ public class Alarm {
      * that should be run.
      */
     public void timerInterrupt() {
-	KThread.currentThread().yield();
+	long currentTime = Machine.timer().getTime();
+	boolean wakeMoreThread = true;
+	while(wakeMoreThread && waitQueue.peek() != null) {
+	    if(waitQueue.peek().getTime() < currentTime) {
+		waitQueue.poll().getThread().ready();
+	    } else {
+		wakeMoreThread = false;
+	    }
+	}
+	KThread.yield();
     }
 
     /**
@@ -45,9 +58,30 @@ public class Alarm {
      * @see	nachos.machine.Timer#getTime()
      */
     public void waitUntil(long x) {
-	// for now, cheat just to get something working (busy waiting is bad)
+	boolean intStatus = Machine.interrupt().disable();
 	long wakeTime = Machine.timer().getTime() + x;
-	while (wakeTime > Machine.timer().getTime())
-	    KThread.yield();
+	waitQueue.add(new ThreadTimeTuple(KThread.currentThread(), wakeTime));
+	KThread.sleep();
+	Machine.interrupt().restore(intStatus);
+    }
+
+    private class ThreadTimeTuple implements Comparable<ThreadTimeTuple> {
+	private KThread thread;
+	private long waitTime;
+
+	ThreadTimeTuple(KThread thread, long waitTime) {
+	    this.thread = thread;
+	    this.waitTime = waitTime;
+	}
+
+	@Override
+	public int compareTo(ThreadTimeTuple other) {
+	    if (this.waitTime > other.waitTime)	return 1;
+	    if (this.waitTime < other.waitTime) return -1;
+	    return 0;
+	}
+	
+	public long getTime() {return this.waitTime;}
+	public KThread getThread() {return this.thread;}
     }
 }
