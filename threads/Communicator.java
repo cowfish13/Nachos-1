bd5c7d638ffe0ea33f51b450cf09df64123e72ae
nbjsw <nbxsjsw@yahoo.com>
2014-02-08 20:54:59 -0800

    Proj_1 Problem 4
    Implement the producer and consumer in Communicator

--- threads/Communicator.java
+++ threads/Communicator.java
@@ -10,10 +10,38 @@ import nachos.machine.*;
  * threads can be paired off at this point.
  */
 public class Communicator {
+    /* this is the word to be listened by listened
+     * the words from other speakers are hidden and
+     * put to this variable when the owner(speaker) are wakened
+     */
+    private int wordToBeListened;
+
+    /*
+     * if this varialbe is turned on, then the newly coming speaker
+     * goes to sleep
+     **/
+    private boolean isWordToBeListened;
+
+
+    private Condition2 speakerCondition;
+    private Condition2 listenerCondition;
+
+    /*
+     * this condition variable is used to pair a speaker and a listener
+     * */
+    private Condition2 listenerSpeakerHandShake;
+    // private handshake
+
+    private Lock lock;
     /**
      * Allocate a new communicator.
      */
     public Communicator() {
+	this.isWordToBeListened = false;
+	this.lock = new Lock();
+	this.speakerCondition = new Condition2(lock);
+	this.listenerCondition = new Condition2(lock);
+	this.listenerSpeakerHandShake = new Condition2(lock);
     }
 
     /**
@@ -27,6 +55,35 @@ public class Communicator {
      * @param	word	the integer to transfer.
      */
     public void speak(int word) {
+	lock.acquire();
+	while(isWordToBeListened)
+	    speakerCondition.sleep();
+
+	this.isWordToBeListened = true;
+	this.wordToBeListened = word;
+
+	/*
+	 * try to wake up one listener if there is one
+	 * but what if on listener?
+	 * well, that is why listenerSpeakerHandShake is present
+	 **/
+	listenerCondition.wake();
+
+	/*
+	 * 1. if there is no listener
+	 *    goes to sleep in listenerSpeakerHandShake
+	 *
+	 * 2. if there is at least one listener
+	 *    still goes to sleep in listenerSpeakerHandShake
+	 *    however, at least one listener is in ready queue due to 
+	 *
+	 *    listenerCondition.wake()
+	 *
+	 *    that listener will immediately wake this speaker up
+	 **/
+	listenerSpeakerHandShake.sleep();
+
+	lock.release();
     }
 
     /**
@@ -36,6 +93,22 @@ public class Communicator {
      * @return	the integer transferred.
      */    
     public int listen() {
-	return 0;
+	int wordTransferred;
+	lock.acquire();
+	while(!isWordToBeListened)
+	    listenerCondition.sleep();
+
+	wordTransferred = this.wordToBeListened;
+	this.isWordToBeListened = false;
+
+	/*
+	 * wake up the speaker who shout `word` out
+	 **/
+	speakerCondition.wake();
+	listenerSpeakerHandShake.wake();
+
+	lock.release();
+
+	return wordTransferred;
     }
 }
